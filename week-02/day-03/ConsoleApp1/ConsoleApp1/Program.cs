257a6cf5e759b3ebd39319478c6277a175948d39
Peter Nemeth <fbpeter0@gmail.com>
2018-02-22 20:34:11 +0100

    added my super project of triangles

---
+++ week-02/day-03/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Create a function that takes a number and a list of numbers as a parameter
+            //  Returns the indeces of the numbers in the list where the first number is part of
+            //  Returns an empty list if the number is not part any of the numbers in the list
+
+            //  Example:
+            Console.WriteLine(SubInt(1, new int[] { 1, 11, 34, 52, 61 }));
+            //  should print: `[0, 1, 4]`
+            Console.WriteLine(SubInt(9, new int[] { 1, 11, 34, 52, 61 }));
+            //  should print: '[]'
+            Console.ReadLine();
+
+        }
+
+        private static StringBuilder SubInt(int number, int[] list)
+        {
+            string stringNumber = number.ToString();
+            StringBuilder results = new StringBuilder();
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i].ToString().Contains(stringNumber))
+                {
+                    results.Append(i + ", ");
+                }
+            }
+            return results;
+
+        }
+    }
+}
---
+++ week-02/day-04/Project - Triangles/Project - Triangles/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project___Triangles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            double x = -25;
+            int y = 800;
+            int z = 50;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 20; j > i; j--)
+                {
+                    Triangle(foxDraw, x, y, z);
+                    x += 50;
+                }
+                y -= 40;
+                x = 75*i;
+                if (x >= 75)
+                {
+                    x -= 50 * i;
+                }
+            }
+        }
+
+        private void Triangle(FoxDraw foxDraw, double x, int y, int z)
+        {
+            foxDraw.DrawLine((50 + x)/2, (100 + y + z)/2, (100 + x)/2, (100 + y + z)/2);
+            foxDraw.DrawLine((50 + x)/2, (100 + y + z)/2, (75 + x)/2, (60 + y + z)/2);
+            foxDraw.DrawLine((100 + x)/2, (100 + y + z)/2, (75 + x)/2, (60 + y + z)/2);
+        }
+       
+
+    }
+}
