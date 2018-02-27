54f00671a3146539e02792e79276ce20182d3faf
Peter Nemeth <fbpeter0@gmail.com>
2018-02-27 17:19:22 +0100

    almost done with the doubled task

---
+++ week-03/day-02/Doubled/Doubled/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Doubled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string log = "duplicated-chars.txt";
+            Decrypt(log);
+            Console.ReadLine();
+        }
+
+        private static void Decrypt(string log)
+        {
+            string[] content = File.ReadAllLines(log);
+            var contentList = new List<char>();
+            foreach (var item in content)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        contentList.Add(item[i]);
+                    }
+                }
+
+            }
+
+            foreach (var item in contentList)
+            {
+                Console.Write(item);
+            }
+
+
+        }
+    }
+}
