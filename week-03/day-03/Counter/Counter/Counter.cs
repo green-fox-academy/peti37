c958fff21960f853f5ab1b82f453ab2420fb3572
Peter Nemeth <fbpeter0@gmail.com>
2018-02-28 17:16:34 +0100

    done with COunter, finally i could understand classes

---
+++ week-03/day-03/Counter/Counter/Counter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    public class Counter
+    {
+        public int start = 0;
+
+        public Counter(int value)
+        {
+            this.start = value;
+        }
+
+        public void Add(int number)
+        {
+            start += number;
+        }
+        public void Add()
+        {
+            start++;
+        }
+        public void Get()
+        {
+            start.ToString();
+        }
+        public void Reset()
+        {
+            start = 0;
+        }
+    }
+
+}
---
+++ week-03/day-03/Counter/Counter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter First = new Counter(37);
+            Console.WriteLine(First.start);
+            First.Add(2);
+            Console.WriteLine(First.start);
+            First.Reset();
+            Console.WriteLine(First.start);
+            First.Add();
+            Console.WriteLine(First.start);
+            First.Get();
+            Console.WriteLine(First.start);
+
+            Console.ReadLine();
+        }
+    }
+}
