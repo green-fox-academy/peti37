57ed9e561e8707c4bd1c59d018ca85ca62ac053f
Peter Nemeth <fbpeter0@gmail.com>
2018-03-07 12:11:08 +0100

    stuck with dominoes

---
+++ week-04/day-03/Dominos/Dominos/Domino.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dominos
+{
+    public class Domino: IComparable<Domino>
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int CompareTo(Domino domino)
+        {
+            return this.Values[0].CompareTo(domino.Values[0]);
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+    }
+}
---
+++ week-04/day-03/Dominos/Dominos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominos
+{
+    class Dominoes
+    {
+        public static void Main(string[] args)
+        {
+            var dominoes = new List<Domino>();
+            dominoes.Add(new Domino(5, 2));
+            dominoes.Add(new Domino(4, 6));
+            dominoes.Add(new Domino(1, 5));
+            dominoes.Add(new Domino(6, 7));
+            dominoes.Add(new Domino(2, 4));
+            dominoes.Add(new Domino(7, 1));
+
+            dominoes.Sort();
+            Console.WriteLine(dominoes[1].CompareTo(dominoes[0]));
+            for (int i = 0; i < dominoes.Count; i++)
+            {
+                foreach (var item in dominoes[i].GetValues())
+                {
+                    Console.Write($"{item} ");
+                }
+            }
+
+            Console.ReadLine();
+            // the order of your dominoes should look like this: [[1,5], [2,4], [4,6], [5,2], [6,7], [7,1]]
+        }
+    }
+}
