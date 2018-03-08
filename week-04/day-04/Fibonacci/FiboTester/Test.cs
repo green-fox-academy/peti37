86dba6d0abd7a9b78f141253f6b2fe9d2e628f97
Peter Nemeth <fbpeter0@gmail.com>
2018-03-08 16:58:04 +0100

    done with fibonaccis

---
+++ week-04/day-04/Fibonacci/FiboTester/Test.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fibonacci;
+
+namespace FiboTester
+{
+    [TestFixture]
+    public class Test
+    {
+        [TestCase]
+        public void TestingFiboIndexes()
+        {
+            Fiber seventeen = new Fibonacci.Fiber();
+            int seventeenth = 17;
+            Assert.AreEqual(1597, seventeen.NFibo(seventeenth));
+        }
+    }
+}
---
+++ week-04/day-04/Fibonacci/Fibonacci/Fiber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class Fiber
+    {
+        public int NFibo(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return NFibo(n - 1) + NFibo(n - 2);
+            }
+        }
+    }
+}
