9f286e6140870d56b4fdcfbb753be70ab518b3ba
Peter Nemeth <fbpeter0@gmail.com>
2018-03-08 11:47:32 +0100

    did my first test

---
+++ week-04/day-04/Apple/Apple/TestAppfel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Compatibility;
+using NUnit.Framework;
+
+namespace Apple
+{
+    [TestFixture]
+    class TestAppfel
+    {
+        [TestCase]
+        public void TestAppfelstring()
+        {
+            Appfel alma = new Appfel();
+            Assert.AreEqual("apple", alma.GetApple());
+        }
+    }
+}
