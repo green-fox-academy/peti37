8138cb62a99116b0d99eeb5d03c8861f46cf82ed
Peter Nemeth <fbpeter0@gmail.com>
2018-03-08 13:57:07 +0100

    started anagram

---
+++ week-04/day-04/Anagram/Test.Anagram/Test.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+
+namespace Test.Anagram
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestMyAnagram()
+        {
+            AnagramStrings First = new AnagramStrings();
+
+        }
+    }
+}
