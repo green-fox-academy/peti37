2d9fb65aa3e89f153b8787f5a2de73db1d55ee0e
Peter Nemeth <fbpeter0@gmail.com>
2018-03-28 17:31:55 +0200

    still stucked

--- week-06/day-03/user_data/main.c
+++ week-06/day-03/user_data/main.c
@@ -14,21 +14,16 @@ int main ()
     char* first_name;
     char* last_name;
     char* age;
-    char* data_array;
+    char** data_array;
 
     printf("Give me your fullname and your age: ");
     gets(data);
-
+    int i = 0;
     for (char *p = strtok(data, " "); p != NULL; p = strtok(NULL, " "))
     {
-        for (int i = 0; p != NULL; i++)
-        {
-            strcpy(data_array[i], p);
-        }
+        first_name = p;
+        i++;
     }
-    first_name = data_array[0];
-    last_name = data_array[1];
-    age = data_array[2];
-    printf("Your name is: %s %s and you are %s years old", first_name, last_name, age);
+    printf("\nYour name is: %s %s and you are %s years old", first_name, last_name, age);
     return 0;
 }
