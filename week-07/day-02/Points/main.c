8224d53f2d1dfbba0624b003e3f0ef615e95f8cf
Peter Nemeth <fbpeter0@gmail.com>
2018-04-03 16:31:20 +0200

    done with points

--- week-07/day-02/Points/main.c
+++ week-07/day-02/Points/main.c
@@ -22,18 +22,26 @@ typedef struct {
     float y;
 } point_t;
 
-point_t creat_point(float, float);
+point_t create_point(float, float);
+float distance(point_t, point_t);
 
 int main()
 {
     point_t p1 = create_point(1, 1);
 	point_t p2 = create_point(1, 5.5);
+	float dist = distance(p1, p2);
+	printf("%.2f", dist);
 	return 0;
 }
 
-point_t creat_point(float x, float y){
+point_t create_point(float x, float y){
     point_t point;
     point.x = x;
     point.y = y;
     return point;
+}
+
+float distance(point_t p1, point_t p2){
+    float distance = sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+    return distance;
 }
