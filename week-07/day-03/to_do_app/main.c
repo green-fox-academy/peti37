403900cc368b2718bc38779dd6ac32d8250a88a7
Peter Nemeth <fbpeter0@gmail.com>
2018-04-04 14:24:06 +0200

    progressed a bit with todo

--- week-07/day-03/to_do_app/main.c
+++ week-07/day-03/to_do_app/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+int counter;
 
 void info(void);
 void commands(void);
+void write_new_task(void);
+
 int main()
 {
     info();
@@ -17,6 +20,7 @@ void info(void){
     printf("Command Line Todo application\n");
     printf("=============================\n\n");
     printf("Command line arguments:\n"
+            "-r   List all the commands\n"
             "-l   Lists all the tasks\n"
             "-a   Adds a new task\n"
             "-r   Removes an task\n"
@@ -24,12 +28,57 @@ void info(void){
 }
 
 void commands(){
+    char comm[2] = "-r";
+    char add[2] = "-a";
     char list[2] = "-l";
-    char* command = (char*)malloc(50);
+
+    char* command = (char*)malloc(2);
     gets(command);
-    if (command[1] == list[1] && command[0] == list[0]){
+
+    if (command[1] == comm[1] && command[0] == comm[0])
         info();
+    else if (command[1] == add[1] && command[0] == add[0])
+        write_new_task();
+    else if (command[1] == list[1] && command[0] == list[0])
+        read_all_tasks();
+    else{
+        system("@cls||clear");
+        printf("Not a valid command, use '-r' for the commands.\n");
     }
-    else
-        printf("Not a valid command");
 }
+
+void write_new_task(void){
+    char task[50];
+    FILE *fptr;
+    fptr = fopen("tasks.txt", "a");
+    printf("Enter new task:\n");
+    gets(task);
+    putc('\n', fptr);
+    fprintf(fptr, "%d. ", counter);
+    fputs("[ ] ", fptr);
+    fputs(task, fptr);
+    fclose(fptr);
+    counter++;
+}
+
+void read_all_tasks(void){
+    FILE *fptr;
+    fptr = fopen("tasks.txt", "r");
+    char single_line[150];
+
+    while(!feof(fptr)){
+        fgets(single_line, 150, fptr);
+        printf("%s", single_line);
+    }
+    fclose(fptr);
+}
+
+void delete_line(void){
+    char index;
+    FILE *fptr;
+    fptr = fopen("tasks.txt", "r+");
+    printf("Enter the index of the task:\n");
+    gets(index);
+}
+
+
