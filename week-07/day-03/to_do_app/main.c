e48efc9a2fef839280d7d780c269ffc11a995512
Peter Nemeth <fbpeter0@gmail.com>
2018-04-04 15:26:10 +0200

    working but the app is getting like a bowl of spagetti

--- week-07/day-03/to_do_app/main.c
+++ week-07/day-03/to_do_app/main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-int counter;
-
 void info(void);
 void commands(void);
 void write_new_task(void);
@@ -20,7 +18,7 @@ void info(void){
     printf("Command Line Todo application\n");
     printf("=============================\n\n");
     printf("Command line arguments:\n"
-            "-r   List all the commands\n"
+            "-i   List all the commands\n"
             "-l   Lists all the tasks\n"
             "-a   Adds a new task\n"
             "-r   Removes an task\n"
@@ -28,9 +26,10 @@ void info(void){
 }
 
 void commands(){
-    char comm[2] = "-r";
+    char comm[2] = "-i";
     char add[2] = "-a";
     char list[2] = "-l";
+    char remove[2] = "-r";
 
     char* command = (char*)malloc(2);
     gets(command);
@@ -41,24 +40,28 @@ void commands(){
         write_new_task();
     else if (command[1] == list[1] && command[0] == list[0])
         read_all_tasks();
+    else if (command[1] == remove[1] && command[0] == remove[0])
+        delete_line();
     else{
         system("@cls||clear");
-        printf("Not a valid command, use '-r' for the commands.\n");
+        printf("Not a valid command, use '-i' for the commands.\n");
     }
 }
 
 void write_new_task(void){
     char task[50];
     FILE *fptr;
-    fptr = fopen("tasks.txt", "a");
+    fptr = fopen("tasks.txt", "a+");
     printf("Enter new task:\n");
     gets(task);
-    putc('\n', fptr);
-    fprintf(fptr, "%d. ", counter);
-    fputs("[ ] ", fptr);
-    fputs(task, fptr);
+    int number_lines = 0;
+    char single_line[150];
+    while(!feof(fptr)){
+        fgets(single_line, 150, fptr);
+        number_lines++;
+    }
+    fprintf(fptr, "\n%d. [ ] %s", number_lines, task);
     fclose(fptr);
-    counter++;
 }
 
 void read_all_tasks(void){
@@ -76,9 +79,28 @@ void read_all_tasks(void){
 void delete_line(void){
     char index;
     FILE *fptr;
-    fptr = fopen("tasks.txt", "r+");
-    printf("Enter the index of the task:\n");
-    gets(index);
+    FILE *fptr_temp;
+    fptr = fopen("tasks.txt", "r");
+    fptr_temp = fopen("tasks_temp.txt", "w");
+    printf("Enter the index of the task:");
+    scanf("%c", &index);
+    char single_line[150];
+    while(!feof(fptr)){
+        fgets(single_line, 150, fptr);
+        if  (single_line[0] != index)
+                fprintf(fptr_temp, "%s", single_line);
+    }
+    fclose(fptr);
+    fclose(fptr_temp);
+    fptr = fopen("tasks.txt", "w");
+    fptr_temp = fopen("tasks_temp.txt", "r");
+    while(!feof(fptr_temp)){
+        fgets(single_line, 150, fptr_temp);
+        fprintf(fptr, "%s", single_line);
+    }
+    fclose(fptr);
+    fclose(fptr_temp);
+
 }
 
 
