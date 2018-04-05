80c8dbf6fe9d0e1e693d7d3dd615c886dfa6f0c6
Peter Nemeth <fbpeter0@gmail.com>
2018-04-05 13:16:03 +0200

    started temp_logger

--- week-07/day-03/to_do_app/main.c
+++ week-07/day-03/to_do_app/main.c
@@ -6,6 +6,7 @@ void info(void);
 void commands(void);
 void write_new_task(void);
 void delete_line(void);
+void delete_line2(void);
 void read_all_tasks(void);
 void read_all_tasks2(void);
 void complete_task(void);
@@ -49,7 +50,7 @@ void commands(){
         read_all_tasks2();
     }
     else if (command[1] == remove[1] && command[0] == remove[0]){
-        delete_line();
+        delete_line2();
         printf("\n");
         read_all_tasks2();
     }
@@ -167,4 +168,23 @@ void read_all_tasks2(void){
     }
 
     fclose(fptr);
+}
+
+void delete_line2(void){
+    printf("Give me the index of the task:\n");
+    int index = 0;
+    int number_lines = 0;
+    puts(index);
+    FILE *fptr;
+    fptr = fopen("tasks.txt", "r+");
+    char single_line[150];
+    fread(single_line, 50, sizeof(fptr), fptr);
+    for (char *p = strtok(single_line, "\n"); p != NULL; p = strtok(NULL, "\n"))
+    {
+        if (index == number_lines)
+            strcpy(p, "");
+        number_lines++;
+    }
+    fclose(fptr);
+
 }
